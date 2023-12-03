8cc70bd4e046d460ae35f9f622e881968e02731b
Oleh Selin <oleh.selin@glassnode.com>
2023-12-03 13:14:09 +0200

    Task 3

--- src/03/part1.ts
+++ src/03/part1.ts
@@ -1,13 +1,13 @@
 import { readFileSync } from 'fs';
 
-type CurrentNumber = {
+type PartNumber = {
     digits: string;
     hasAdjacentSymbol: boolean;
 }
 
 const file = readFileSync('full.txt', 'utf8');
 const eng = file.split('\n');
-const l = eng[0].length;
+const w = eng[0].length;
 const h = eng.length;
 
 function isSymbol(x: number, y: number): boolean {
@@ -29,22 +29,22 @@ function hasAdjacentSymbol(x: number, y: number): boolean {
 
 let sum = 0;
 let inNumber = false;
-let curNum: CurrentNumber = { digits: '', hasAdjacentSymbol: false };
+let curPartNum: PartNumber = { digits: '', hasAdjacentSymbol: false };
 for (let y = 0; y < h; y += 1) {
-    for (let x = 0; x <= l; x += 1) {
+    for (let x = 0; x <= w; x += 1) {
         if (isNumber(x, y)) {
             if (!inNumber) {
                 inNumber = true;
-                curNum = { digits: '', hasAdjacentSymbol: false };
+                curPartNum = { digits: '', hasAdjacentSymbol: false };
             }
 
-            curNum.digits += eng[y][x];
-            curNum.hasAdjacentSymbol ||= hasAdjacentSymbol(x, y);
+            curPartNum.digits += eng[y][x];
+            curPartNum.hasAdjacentSymbol ||= hasAdjacentSymbol(x, y);
         } else {
             if (inNumber) {
                 inNumber = false;
-                if (curNum.hasAdjacentSymbol) {
-                    sum += parseInt(curNum.digits, 10);
+                if (curPartNum.hasAdjacentSymbol) {
+                    sum += parseInt(curPartNum.digits, 10);
                 }
             }
         }
